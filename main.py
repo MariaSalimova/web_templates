affd88d2d7fbf94c7703ad1d7152fcb3a43f1248
Maria <mashasalimova777@gmail.com>
2022-02-19 16:10:04 +0500

    задача 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,15 @@ app = Flask(__name__)
 @app.route('/index/<title>')
 def index(title):
     return render_template('base.html', title=title)
+
+
+@app.route('/trainig/<prof>')
+def training(prof):
+    if 'инженер' or 'строитель' in prof.lower():
+        return render_template('trainig.html', title='Инженерные тренажеры',
+                               image=url_for('static', filename='pictures/eng.jpg'))
+    return render_template('trainig.html', title='Научные симуляторы',
+                           image=url_for('static', filename='pictures/sci.jpg'))
 
 
 if __name__ == '__main__':
